da995975bcd9b6d59e73bf7a031b7dfe7b3b277a
Oscar Pacheco <oscarpa_2000@hotmail.com>
2021-11-15 23:45:09 -0500

    jugabilidad final terminada

--- policia y ladron.cpp
+++ policia y ladron.cpp
@@ -12,21 +12,21 @@ const int MAXF = 100, MAXC = 100;
 //Estructuras
 struct sMovimiento
 {
-    string direccion; //Arriba, abajo, izquierda o derecha
-    int movimiento; //Entre el 1 y el 6
+    string direccion{}; //Arriba, abajo, izquierda o derecha
+    int movimiento{}; //Entre el 1 y el 6
 };
 
 struct sArma
 {
-    string nombre; //Nombre del arma usada
-    double contundencia; //Letalidad del arma usada
-    double probabilidad; //Probabilidad de impacto del arma
+    string nombre{}; //Nombre del arma usada
+    double contundencia{}; //Letalidad del arma usada
+    double probabilidad{}; //Probabilidad de impacto del arma
 };
 
 struct sJugador
 {
-    int posF; //Posición en y
-    int posC; //Posición en x
+    int posF{}; //Posición en y
+    int posC{}; //Posición en x
     sArma armas[2]; //Dos armas del jugador
     double energia = 30; //Energia del jugador
 };
@@ -39,6 +39,8 @@ sArma inicializarArmas(sArma arma);
 sJugador inicializarPolicia(int filas, int columnas);
 sJugador inicializarLadron();
 void movimiento(string tablero[][MAXC], int filas, int columnas, sJugador& policia, sJugador& ladron, bool& turno, sMovimiento& dado);
+void duelo(sJugador& policia, sJugador& ladron);
+double ataque(sArma arma, sMovimiento dado);
 
 //Función principal
 int main()
@@ -48,41 +50,66 @@ int main()
     sMovimiento dado;
     bool turno = false; //False para ladrón, true para policia
     int filas, columnas, salida = 0;
-    cout << "Ingrese el numero de filas del tablero: ";
-    cin >> filas;
-    cout << "Ingrese el numero de columnas del tablero: ";
-    cin >> columnas;
-    system("cls");
+    do
+    {
+        cout << "Ingrese el numero de filas del tablero (mayor a 5): ";
+        cin >> filas;
+        cout << "Ingrese el numero de columnas del tablero (mayor a 5): ";
+        cin >> columnas;
+        std::system("cls");
+    } while ((filas < 6 || filas > 99) || (columnas < 6 || columnas > 99));
     sJugador policia = inicializarPolicia(filas,columnas);
     sJugador ladron = inicializarLadron();
     inicializarTablero(tablero, filas, columnas);
     imprimirTablero(tablero, filas, columnas, policia, ladron);
-    
+    std::system("pause");
+    std::system("cls");
 
     //Ejecución del juego
     while (salida != 1)
     {
         movimiento(tablero, filas, columnas, policia, ladron, turno, dado);
 
-        //Si se queda alguien sin energia
-        if (policia.energia<0)
+        
+        
+        imprimirTablero(tablero, filas, columnas, policia, ladron);
+        if (!turno)
         {
+            cout << "El policia se ha movido hacia: " << dado.direccion << endl;
+            cout << "El policia se ha movido " << dado.movimiento << " casillas" << endl;            
+        }
+        else
+        {
+            cout << "El ladron se ha movido hacia: " << dado.direccion << endl;
+            cout << "El ladron se ha movido " << dado.movimiento << " casillas" << endl;
+        }
+        std::system("pause");
+        std::system("cls");
+
+        //Si entra en modo duelo
+        for (int i = policia.posF - 1; i < policia.posF + 1; i++) {
+            for (int j = policia.posC - 1; i < policia.posC + 1; j++) {
+                if (ladron.posF == i && ladron.posC == j) {
+                    cout << "\n\n\t SE HA ENTRADO EN MODO DUELO!" << endl;
+                    std::system("pause");
+                    std::system("cls");
+                    duelo(policia, ladron);
+                }
+            }
+        }
+        //Si se queda alguien sin energia
+        if (policia.energia < 0)
+        {
+            cout << "\n\n\n\n\n\t\t\tEL GANADOR ES EL LADRON!!"<<endl;
             salida = 1;
         }
         else if (ladron.energia < 0)
         {
+            cout << "\n\n\n\n\n\t\t\tEL GANADOR ES EL POLICIA!!" << endl;
             salida = 1;
         }
-
-        //Si entra en modo duelo
-        if (true)
-        {
-
-        }
-        imprimirTablero(tablero, filas, columnas, policia, ladron);
     }
-
-    //Pantalla de ganador
+    return 0;
 }
 
 //Inicia el tablero
@@ -97,7 +124,7 @@ void inicializarTablero(string tablero [][MAXC], int filas, int columnas)
         }
     }
     tablero[0][0] = "L";
-    tablero[filas - 1][columnas - 1] = "P";
+    tablero[filas - 1][(columnas - 1)] = "P";
     
 }
 
@@ -116,8 +143,6 @@ void imprimirTablero(string tablero[][MAXC], int filas, int columnas, sJugador p
     cout << endl << endl;
     cout << "Energia del ladron: " << ladron.energia <<endl;
     cout << "Energia del policia: " << policia.energia <<endl;
-    system("pause");
-    system("cls");
 }
 
 //Entrega los dados con una direccion y un numero de posiciones
@@ -288,13 +313,13 @@ void movimiento(string tablero[][MAXC], int filas, int columnas, sJugador &polic
                 {
                     movimientosRestantes *= -1;
                 }
-                //Movimiento hacia arriba
+                //Movimiento hacia izquierda
                 if (movimientosRestantes < 0)
                 {
                     policia.posC--;
                     movimientosRestantes++;
                 }
-                //Movimiento hacia abajo
+                //Movimiento hacia derecha
                 else if (movimientosRestantes > 0)
                 {
                     policia.posC++;
@@ -458,7 +483,7 @@ void movimiento(string tablero[][MAXC], int filas, int columnas, sJugador &polic
             }
             //Actualización del tablero
             tablero[ladron.posF][posAntigua] = "x";
-            tablero[ladron.posF][ladron.posC] = "L"; //remplazo de letra por el ladron
+            tablero[(ladron.posF)][(ladron.posC)] = "L"; //remplazo de letra por el ladron
         }
         else //Derecha
         {
@@ -501,6 +526,70 @@ void movimiento(string tablero[][MAXC], int filas, int columnas, sJugador &polic
     //En caso de que queden en la misma posición
     if (policia.posC == ladron.posC && policia.posF == ladron.posF)
     {
-        tablero[policia.posF][policia.posC] = "P-L";
+        tablero[(policia.posF)][policia.posC] = "P-L";
+    }
+}
+
+void duelo(sJugador& policia, sJugador& ladron) 
+{
+    sMovimiento dado;
+    int arma;
+    double dmg;
+    while (policia.energia > 0 && ladron.energia > 0) {
+        dado = dados();
+        //Attack del policia
+        if (policia.energia > 0)
+        {
+            cout << "\n\n\t\tTURNO DEL POLICIA \n\n" << endl;
+            cout << "Energia del ladron: " << ladron.energia << endl;
+            cout << "Energia del policia: " << policia.energia << endl;
+            cout << "\n\nEscoja un arma: digite 0 si quiere "<<policia.armas[0].nombre<<" o 1 si quiere "<<policia.armas[1].nombre<<": ";
+            cin >> arma;
+            arma >= 1 ? arma = 1 : arma = 0; //Caso de validación, en caso que el usuario ingrese un numero incorrecto
+            dmg = ataque(policia.armas[arma], dado);
+            cout << "\n\nAtacando..." << endl;
+            if (dmg == 0) {
+                cout << "\n\n\tEL ATAQUE HA FALLADO" << endl << endl;
+            }
+            else {
+                cout << "\n\n\tEL ATAQUE HA INFLINGIDO "<<dmg<<" de danio. "<<endl<<endl;
+                ladron.energia -= dmg;
+            }
+            std::system("pause");
+            std::system("cls");
+        }
+        dado = dados();
+        //Attack del ladron
+        if (ladron.energia > 0) {
+            cout << "\n\n\t\tTURNO DEL LADRON \n\n" << endl;
+            cout << "Energia del ladron: " << ladron.energia << endl;
+            cout << "Energia del policia: " << policia.energia << endl;
+            arma = rand() % (1);
+            cout << "\n\nSe ha escogido el arma " << ladron.armas[arma].nombre << endl;
+            dmg = ataque(ladron.armas[arma], dado);
+            cout << "\n\nAtacando..." << endl;
+            if (dmg == 0) {
+                cout << "\n\n\tEL ATAQUE HA FALLADO" << endl << endl;
+            }
+            else {
+                cout << "\n\n\tEL ATAQUE HA INFLINGIDO " << dmg << " de danio. " << endl << endl;
+                policia.energia -= dmg;
+            }
+            std::system("pause");
+            std::system("cls");
+        }
+    }
+}
+
+//Retorna la energia restada al oponente, en caso de fallar es 0
+double ataque(sArma arma, sMovimiento dado) 
+{
+    int probabilidad = 1 + rand() % (100);
+    if (arma.probabilidad*100<probabilidad)//Cuando falla
+    {
+        return 0;
+    }
+    else {
+        return dado.movimiento * arma.contundencia;
     }
 }
